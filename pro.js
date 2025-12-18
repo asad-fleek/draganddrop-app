aaebb3276986612d2265e0e533264ae681003c96
asad-fleek <asad.mahmood@pendoah.ai>
2025-12-18 20:58:11 +0500

    project completed

--- pro.js
+++ pro.js
@@ -1,25 +1,41 @@
 let lists = document.querySelectorAll(".list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
+let fileBox = document.getElementById("files");
+
+let selected = null;
+
 
 lists.forEach(item => {
-    item.addEventListener("dragstart", function (e) {
+    item.addEventListener("dragstart", (e) => {
         selected = e.target;
     });
 });
-rightBox.addEventListener("dragover", function (e) {
+
+[rightBox, leftBox].forEach(box => {
+    box.addEventListener("dragover", e => e.preventDefault());
+    box.addEventListener("drop", () => {
+        if (selected) {
+            box.appendChild(selected);
+            selected = null;
+        }
+    });
+});
+
+
+fileBox.addEventListener("dragover", (e) => {
     e.preventDefault();
 });
 
-leftBox.addEventListener("dragover", function (e) {
+fileBox.addEventListener("drop", (e) => {
     e.preventDefault();
-});
-rightBox.addEventListener("drop", function (e) {
-    rightBox.appendChild(selected);
-    selected = null;
-});
 
-leftBox.addEventListener("drop", function (e) {
-    leftBox.appendChild(selected);
-    selected = null;
+    const files = e.dataTransfer.files;
+
+    for (let file of files) {
+        const div = document.createElement("div");
+        div.className = "file-item";
+        div.textContent = file.name;
+        fileBox.appendChild(div);
+    }
 });
